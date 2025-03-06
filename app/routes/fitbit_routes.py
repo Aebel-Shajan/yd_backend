af3bd2403a8b04df8b717f2c8d541a781b1afe7e
aebel-shajan <aebelshajan@gmail.com>
2025-03-06 11:46:42 +0000

    Add routes for steps, exercise and sleep.

--- app/routes/fitbit_routes.py
+++ app/routes/fitbit_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 
-from app.models import ActivityMetaData, CalorieActivity, ValueColMetaData
+from app.models import ActivityMetaData, CalorieActivity, ExerciseActivity, SleepActivity, StepActivity, ValueColMetaData
 from app.routes.utils import get_activities
 from app.services.fitbit_service import handle_fitbit_zip
 
@@ -40,3 +40,66 @@ def get_calories_activities(year: str):
         model=CalorieActivity,
         metadata=metadata
     )
+    
+@fitbit_bp.route("/steps/<path:year>", methods=["GET"])
+def get_step_activities(year: str):
+    metadata=ActivityMetaData(
+        date_col="date",
+        value_cols=[
+            ValueColMetaData(
+                col="value",
+                units="steps"
+            )
+        ],
+        filter_cols=[],
+    )
+    return get_activities(
+        year=year,
+        model=StepActivity,
+        metadata=metadata
+    )
+    
+@fitbit_bp.route("/exercise/<path:year>", methods=["GET"])
+def get_exercise_activities(year: str):
+    metadata=ActivityMetaData(
+        date_col="date",
+        value_cols=[
+            ValueColMetaData(
+                col="active_duration",
+                units="milliseconds"
+            ),
+            ValueColMetaData(
+                col="average_heart_rate",
+                units="bpm"
+            ),
+            ValueColMetaData(
+                col="calories",
+                units="calores"
+            )
+        ],
+        filter_cols=["activity_name"],
+    )
+    return get_activities(
+        year=year,
+        model=ExerciseActivity,
+        metadata=metadata
+    )
+    
+@fitbit_bp.route("/sleep/<path:year>", methods=["GET"])
+def get_sleep_activities(year: str):
+    metadata=ActivityMetaData(
+        date_col="date",
+        value_cols=[
+            ValueColMetaData(
+                col="total_duration_hours",
+                units="hours"
+            )
+        ],
+        filter_cols=[],
+    )
+    return get_activities(
+        year=year,
+        model=SleepActivity,
+        metadata=metadata
+    )
+    
