8fbc746eece3bcc15ca77db91cdfd5463430b423
aebel-shajan <aebelshajan@gmail.com>
2025-03-16 11:53:48 +0000

    Add logic to make sure only I can post stuff to the api.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request, session
 from app.database import init_db
 from app.routes.workout_routes import workout_bp
 from app.routes.reading_routes import reading_bp
@@ -8,22 +8,42 @@ from app.config import Config  # Instead of "config"
 import os
 
 
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = "super secret key"
-    app.config.from_object(Config)  
+app = Flask(__name__)
+app.secret_key = "super secret key"
+app.config.from_object(Config)  
 
+init_db()
 
-    init_db()
+app.register_blueprint(workout_bp, url_prefix="/workouts")
+app.register_blueprint(reading_bp, url_prefix="/reading")
+app.register_blueprint(fitbit_bp, url_prefix="/fitbit")
+app.register_blueprint(github_bp, url_prefix="/github")
 
-    app.register_blueprint(workout_bp, url_prefix="/workouts")
-    app.register_blueprint(reading_bp, url_prefix="/reading")
-    app.register_blueprint(fitbit_bp, url_prefix="/fitbit")
-    app.register_blueprint(github_bp, url_prefix="/github")
+@app.before_request
+def check_github_auth():
+    """Ensure authenticated with github and username is correct."""
+    if request.method == "POST":
+        try:
+            token = session["github_token"]
+            username = session["github_username"]
+        except KeyError:
+            return "error: Authenticate as Aebel-Shajan with github first before making post calls!", 400
     
+        if username != "Aebel-Shajan":
+            return "error: Only github user Aebel-Shajan can POST to this api!", 400
 
-    return app
+
+@app.route("/", methods=["GET"])
+def home():
+    routes = []
+    for rule in app.url_map.iter_rules():
+        if "static" in rule.endpoint:  # Skip static files
+            continue
+        routes.append({
+            "route": str(rule),
+            "methods": list(rule.methods - {"OPTIONS", "HEAD"})  # Remove unwanted methods
+        })
+    return jsonify(routes), 200
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
--- app/routes/github_routes.py
+++ app/routes/github_routes.py
@@ -48,20 +48,33 @@ def github_callback():
         return "Error: Could not get access token", 400
 
     access_token = token_data["access_token"]
-    session["github_token"] = access_token  # Store in session
+    
+    
+    # Get username
+    response = requests.post(
+        url="https://api.github.com/user",
+        headers={
+            "Authorization": f"token {access_token}"
+        }
+    )
+    user_data = response.json()
+    if "login" not in user_data:
+        return "Error: Could not get github username", 400
+    
+    username = user_data["login"]
+    
+    if username != "Aebel-Shajan":
+        return f"Error: Only aebel-shajan can access this api! sos {username}"
 
+    # Store in session
+    session["github_username"] = username
+    session["github_token"] = access_token 
     return "Success: Successfully authenticated with github.", 200
 
 
 @github_bp.route("/<int:year>", methods=["POST"])
 def retrieve_github_activity(year: int):
-    try:
-        github_token = session["github_token"]
-        if github_token is None:
-            raise Exception("Error no github token found!")
-    except:
-        return jsonify({"error": "Authenticate first!"}), 401
-    df = process_repo_contributions(github_token ,year)
+    df = process_repo_contributions(session["github_token"] ,year)
     output = add_activities_df_to_db(df, GithubActivity)
     return jsonify(output), 201
 
