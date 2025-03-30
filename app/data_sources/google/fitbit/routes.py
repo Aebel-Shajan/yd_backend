9ac18bf31461f4b2e015901760bcae612b82e787
aebel-shajan <aebelshajan@gmail.com>
2025-03-30 22:14:17 +0100

    Add get routes for sleep, steps, exercises.

--- app/data_sources/google/fitbit/routes.py
+++ app/data_sources/google/fitbit/routes.py
@@ -1,41 +1,73 @@
-
-
 from fastapi import APIRouter, Depends, HTTPException
 from httplib2 import Credentials
 import pandas as pd
 
 from app.auth.services import get_current_user_credentials
-from app.drive.services import download_file, query_drive_file, query_or_create_nested_folder
+from app.drive.services import get_data_from_csv
 
 
 router = APIRouter(prefix="/fitbit")
 
 
 @router.get("/calories/{year}")
-async def get_calorie_data(
+async def get_fitbit_calories_data(
     year: int,
     credentials: Credentials = Depends(get_current_user_credentials),
 ):
-    output_folder_id = query_or_create_nested_folder(credentials, "year-in-data/outputs")
-    data_file_id = query_drive_file(
-        credentials, 
-        name="fitbit_calories.csv", 
-        parent_id=output_folder_id
+    data = get_data_from_csv(
+        credentials,
+        csv_name="fitbit_calories.csv",
+        year=year,
     )
-    print(data_file_id)
-    if data_file_id:    
-        file = download_file(credentials, data_file_id)
-        df = pd.read_csv(file)
-        # Ensure the date column is in datetime format 
-        df['date'] = pd.to_datetime(df['date'], errors='coerce')
-        df = df[df['date'].dt.year == year]
-        # Replace nan values
-        df = df.fillna("")
-        data  = df.to_dict(orient='records')
-        return {
-            "status": "success",
-            "data": data
-        }
-    
+    if data:
+        return {"status": "success", "data": data}
+
     raise HTTPException(400, detail="Data file for data source not found!")
-    
+
+
+@router.get("/steps/{year}")
+async def get_fitbit_steps_data(
+    year: int,
+    credentials: Credentials = Depends(get_current_user_credentials),
+):
+    data = get_data_from_csv(
+        credentials,
+        csv_name="fitbit_steps.csv",
+        year=year,
+    )
+    if data:
+        return {"status": "success", "data": data}
+
+    raise HTTPException(400, detail="Data file for data source not found!")
+
+
+@router.get("/sleep/{year}")
+async def get_fitbit_sleep_data(
+    year: int,
+    credentials: Credentials = Depends(get_current_user_credentials),
+):
+    data = get_data_from_csv(
+        credentials,
+        csv_name="fitbit_sleep.csv",
+        year=year,
+    )
+    if data:
+        return {"status": "success", "data": data}
+
+    raise HTTPException(400, detail="Data file for data source not found!")
+
+
+@router.get("/exercises/{year}")
+async def get_fitbit_exercises_data(
+    year: int,
+    credentials: Credentials = Depends(get_current_user_credentials),
+):
+    data = get_data_from_csv(
+        credentials,
+        csv_name="fitbit_exercises.csv",
+        year=year,
+    )
+    if data:
+        return {"status": "success", "data": data}
+
+    raise HTTPException(400, detail="Data file for data source not found!")
