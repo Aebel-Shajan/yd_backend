8329588f57c8dd53c986eca268bfd4b975a8604a
aebel-shajan <aebelshajan@gmail.com>
2025-03-30 16:22:12 +0100

    Add yd_extractor module.

---
+++ yd_extractor/fitbit/calories.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import shutil
+import pandas as pd
+
+from yd_extractor.utils.utils import extract_specific_files_flat
+
+from .utils import extract_json_file_data, transform_time_series_data
+
+
+def process_calories(
+    inputs_folder: Path,
+    zip_path: Path,
+    cleanup: bool=True
+) -> pd.DataFrame:
+    """Extract calories from folder then apply some transformations on data."""
+    
+    # Unzip and extract calories jsons from zip file.
+    data_folder = inputs_folder / "calories"
+    extract_specific_files_flat(
+        zip_file_path=zip_path,
+        prefix="Takeout/Fitbit/Global Export Data/calories",
+        output_path=data_folder
+    )
+    df_raw = extract_json_file_data(
+        folder_path=data_folder,
+        file_name_prefix="calories",
+        keys_to_keep=["dateTime", "value"]
+    )
+    
+    df_transformed = transform_time_series_data(df=df_raw)
+    
+    if cleanup:
+        shutil.rmtree(data_folder)
+        
+    return df_transformed
