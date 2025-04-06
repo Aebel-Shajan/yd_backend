cb0f53d1e4d5b35f4a165c59d303cf85356c5480
aebel-shajan <aebelshajan@gmail.com>
2025-04-06 12:10:13 +0100

    Refactor upload_google_takeout_data to use sqlite db instead of google sheets.

--- app/data_sources/google/routes.py
+++ app/data_sources/google/routes.py
@@ -1,15 +1,20 @@
 import os
 import pathlib
 import shutil
-from fastapi import APIRouter, Depends, UploadFile, HTTPException
-from google.oauth2.credentials import Credentials
+from fastapi import APIRouter, UploadFile, HTTPException
 
-from app.auth.services import get_current_user_credentials
 from app.data_sources.services import check_folder_exists_in_zip
-from app.drive.services import create_or_update_sheet, query_or_create_nested_folder, upload_or_overwrite
 from app.config import Config
 from yd_extractor import fitbit
 from app.data_sources.google.fitbit.routes import router as fitbit_router
+from app.database.session import SessionLocal
+from app.data_sources.services import upload_df_to_table
+from app.data_sources.google.fitbit.models import (
+    FitbitCalories, 
+    FitbitExercise, 
+    FitbitSleep,
+    FitbitSteps
+)
 
 router = APIRouter(prefix="/google")
 
@@ -17,10 +22,7 @@ router.include_router(fitbit_router)
 
 
 @router.post("/upload-data")
-async def upload_google_takeout_data(
-    file: UploadFile,
-    credentials: Credentials = Depends(get_current_user_credentials),
-):
+async def upload_google_takeout_data(file: UploadFile):
     
     # Validate zip
     if not file.filename.endswith(".zip"):
@@ -30,7 +32,6 @@ async def upload_google_takeout_data(
         )
         
     # try:
-    output_folder_id = query_or_create_nested_folder(credentials, "year-in-data/outputs")
     zip_file_path = pathlib.Path(Config.UPLOAD_FOLDER) / file.filename
     with zip_file_path.open("wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
@@ -38,72 +39,103 @@ async def upload_google_takeout_data(
     
     # Fitbit
     if check_folder_exists_in_zip(zip_file_path, "Takeout/Fitbit/Global Export Data"):
-        uploaded_sources.append("fitbit")
-        # Calories
-        df = fitbit.process_calories(
-            inputs_folder=pathlib.Path(Config.UPLOAD_FOLDER),
-            zip_path=zip_file_path
-        )
-        create_or_update_sheet(
-            credentials,
-            df=df,
-            worksheet_name="fitbit_calories",
-            file_name="year_in_data",
-            parent_id=output_folder_id
-        )
+        extracted_data = []
         
-        # Sleep
-        df = fitbit.process_sleep(
-            inputs_folder=pathlib.Path(Config.UPLOAD_FOLDER),
-            zip_path=zip_file_path
-        )
-        create_or_update_sheet(
-            credentials,
-            df=df,
-            worksheet_name="fitbit_sleep",
-            file_name="year_in_data",
-            parent_id=output_folder_id
-        )
-        
-        # Exercises
-        df = fitbit.process_exercise(
-            inputs_folder=pathlib.Path(Config.UPLOAD_FOLDER),
-            zip_path=zip_file_path
-        )
-        create_or_update_sheet(
-            credentials,
-            df=df,
-            worksheet_name="fitbit_exercises",
-            file_name="year_in_data",
-            parent_id=output_folder_id
-        )
-        
-        # Steps
-        df = fitbit.process_steps(
-            inputs_folder=pathlib.Path(Config.UPLOAD_FOLDER),
-            zip_path=zip_file_path
-        )
-        create_or_update_sheet(
-            credentials,
-            df=df,
-            worksheet_name="fitbit_steps",
-            file_name="year_in_data",
-            parent_id=output_folder_id
-        )
-        
+        with SessionLocal() as db:
+            # Calories
+            df = fitbit.process_calories(
+                inputs_folder=pathlib.Path(Config.UPLOAD_FOLDER),
+                zip_path=zip_file_path
+            )
+            added_rows, duplicate_rows = upload_df_to_table(
+                df=df,
+                model=FitbitCalories,
+                db=db
+            )
+            extracted_data.append(
+                {
+                    "table_name": "fitbit_calories",
+                    "added_rows": added_rows,
+                    "duplicate_rows": duplicate_rows
+                }
+            )
+            
+            # Sleep
+            df = fitbit.process_sleep(
+                inputs_folder=pathlib.Path(Config.UPLOAD_FOLDER),
+                zip_path=zip_file_path
+            )
+            added_rows, duplicate_rows = upload_df_to_table(
+                df=df,
+                model=FitbitSleep,
+                db=db
+            )
+            extracted_data.append(
+                {
+                    "table_name": "fitbit_sleep",
+                    "added_rows": added_rows,
+                    "duplicate_rows": duplicate_rows
+                }
+            )
+            
+            # Exercises
+            df = fitbit.process_exercise(
+                inputs_folder=pathlib.Path(Config.UPLOAD_FOLDER),
+                zip_path=zip_file_path
+            )
+            added_rows, duplicate_rows = upload_df_to_table(
+                df=df,
+                model=FitbitExercise,
+                db=db
+            )
+            extracted_data.append(
+                {
+                    "table_name": "fitbit_exercises",
+                    "added_rows": added_rows,
+                    "duplicate_rows": duplicate_rows
+                }
+            )
+            
+            # Steps
+            df = fitbit.process_steps(
+                inputs_folder=pathlib.Path(Config.UPLOAD_FOLDER),
+                zip_path=zip_file_path
+            )
+            added_rows, duplicate_rows = upload_df_to_table(
+                df=df,
+                model=FitbitSteps,
+                db=db
+            )
+            extracted_data.append(
+                {
+                    "table_name": "fitbit_steps",
+                    "added_rows": added_rows,
+                    "duplicate_rows": duplicate_rows
+                }
+            )
+            
+            uploaded_sources.append(
+                {
+                    "source": "fitbit",
+                    "extracted_data": extracted_data
+                }
+            )
     
     # Youtube
     if check_folder_exists_in_zip(zip_file_path, "Takeout/YouTube and YouTube Music/history"):
-        uploaded_sources.append("youtube")
+        uploaded_sources.append(
+            {
+                "source": "youtube",
+                "extracted_data": []
+            }
+        )
         
     os.remove(zip_file_path)
     
     return {
         "status": "success",
         "message": "Successfully uploaded and processed google takeout data.",
-        "data": {
-            "uploaded_sources": uploaded_sources
-        }
+        "data": uploaded_sources
     }
     # except Exception as e:
     #     raise HTTPException(status_code=500, detail=f"Failed to upload file: {e}")
