180bfcb980ed84c18d2f122b88c3c0ced6bbf260
aebel-shajan <aebelshajan@gmail.com>
2025-04-06 13:36:03 +0100

    Update get_fitbit_data route.

--- app/data_sources/google/fitbit/routes.py
+++ app/data_sources/google/fitbit/routes.py
@@ -1,114 +1,50 @@
-from fastapi import APIRouter, Depends, HTTPException
-from httplib2 import Credentials
-import pandas as pd
-
-from app.auth.services import get_current_user_credentials
-from app.drive.services import get_data_from_csv, get_data_from_sheet, query_or_create_nested_folder
+from typing_extensions import Literal
+from fastapi import APIRouter, HTTPException
+from app.database.session import SessionLocal
+from app.data_sources.services import get_data_from_table
+from app.data_sources.google.fitbit.models import (
+    FitbitCalories,
+    FitbitExercise,
+    FitbitSleep,
+    FitbitSteps
+)
 
 
 router = APIRouter(prefix="/fitbit")
 
+ROUTE_MAP = {
+    "calories":    FitbitCalories, 
+    "steps":    FitbitSteps,
+    "sleep":     FitbitSleep,
+    "exercises" :   FitbitExercise,
+}
+routes = list(ROUTE_MAP.keys())
 
-@router.get("/calories/{year}")
-async def get_fitbit_calories_data(
+@router.get("/{data_route}/{year}")
+async def get_fitbit_data(
+    data_route: Literal[*routes],
     year: int,
-    credentials: Credentials = Depends(get_current_user_credentials),
 ):
-    outputs_folder_id = query_or_create_nested_folder(credentials, "year-in-data/outputs")
-    data, metadata = get_data_from_sheet(
-        credentials=credentials,
-        worksheet_name="fitbit_calories",
-        file_name="year_in_data",
-        parent_id=outputs_folder_id,
-        year=year
-    )
+
+    if data_route not in ROUTE_MAP.keys():
+        raise HTTPException(404, "Route not found. Must be one of {allowed_routes}")
     
-    if data:
-        return {
-            "status": "success",
-            "data": data,
-            "metadata": metadata
-        }
+    with SessionLocal() as db:
+        data, metadata = get_data_from_table(
+            model=ROUTE_MAP[data_route],
+            db=db,
+            year=year
+        )
+
+        if data:
+            return {
+                "status": "success",
+                "data": data,
+                "metadata": metadata
+            }
     
 
 
     raise HTTPException(400, detail="Data file for data source not found!")
 
 
-@router.get("/steps/{year}")
-async def get_fitbit_steps_data(
-    year: int,
-    credentials: Credentials = Depends(get_current_user_credentials),
-):
-    outputs_folder_id = query_or_create_nested_folder(credentials, "year-in-data/outputs")
-    data, metadata = get_data_from_sheet(
-        credentials=credentials,
-        worksheet_name="fitbit_steps",
-        file_name="year_in_data",
-        parent_id=outputs_folder_id,
-        year=year,
-    )
-    
-    if data:
-        return {
-            "status": "success",
-            "data": data,
-            "metadata": metadata
-        }
-    
-
-
-    raise HTTPException(400, detail="Data file for data source not found!")
-
-
-@router.get("/sleep/{year}")
-async def get_fitbit_sleep_data(
-    year: int,
-    credentials: Credentials = Depends(get_current_user_credentials),
-):
-    outputs_folder_id = query_or_create_nested_folder(credentials, "year-in-data/outputs")
-    
-    data, metadata = get_data_from_sheet(
-        credentials=credentials,
-        worksheet_name="fitbit_sleep",
-        file_name="year_in_data",
-        parent_id=outputs_folder_id,
-        year=year
-    )
-    
-    if data:
-        return {
-            "status": "success",
-            "data": data,
-            "metadata": metadata
-        }
-    
-
-
-    raise HTTPException(400, detail="Data file for data source not found!")
-
-
-@router.get("/exercises/{year}")
-async def get_fitbit_exercises_data(
-    year: int,
-    credentials: Credentials = Depends(get_current_user_credentials),
-):
-    outputs_folder_id = query_or_create_nested_folder(credentials, "year-in-data/outputs")
-    
-    data, metadata = get_data_from_sheet(
-        credentials=credentials,
-        worksheet_name="fitbit_exercises",
-        file_name="year_in_data",
-        parent_id=outputs_folder_id,
-        year=year
-    )
-    
-    if data:
-        return {
-            "status": "success",
-            "data": data,
-            "metadata": metadata
-        }
-    
-
-    raise HTTPException(400, detail="Data file for data source not found!")
