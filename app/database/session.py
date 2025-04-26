e0658e4a66bd39e322d8ec75d6bb6f9030cceaaa
aebel-shajan <aebelshajan@gmail.com>
2025-04-26 19:35:51 +0100

    use environment variable for database url when possible.

--- app/database/session.py
+++ app/database/session.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./year_in_data.db"  # Replace with your DB connection string
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./year_in_data.db")  # Default to SQLite if not set
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})  # SQLite-specific
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
